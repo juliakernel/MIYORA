d90fb1834c93e65ca1588f4aec1a5cc1f3a02c5a
Janglee <tai@bearabyte.io>
2025-07-18 21:44:02 +0700

    first commit 4

--- src/components/settings/CommunityPage.tsx
+++ src/components/settings/CommunityPage.tsx
@@ -14,7 +14,7 @@ export function CommunityPage() {
       <ul role="list" className="divide-y divide-gray-100 max-w-xs">
         <li className="py-4">
           <a
-            href="https://t.me/arbius_ai"
+            href="https://x.com/MiyoraAI"
             target="_blank"
             className="rounded bg-indigo-600 px-2 py-1 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
@@ -22,6 +22,6 @@ export function CommunityPage() {
           </a>
         </li>
       </ul>
-    </BasicPage>
+    </BasicPage >
   );
 }
